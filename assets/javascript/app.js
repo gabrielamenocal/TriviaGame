428ba85d04f20e9fc499e3d8353814ff95aa154d
Gabriela Menocal <gmenocal89@gmail.com>
2018-08-01 22:43:20 -0400

    done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,150 +4,121 @@
 // q.2 Which country drinks most coffee? a. Finland
 
 // q3. Which if these is countries is a coffee productor? a. Honduras
-var rigths = [];
-var wrongs = [];
-var correct = 0; incorrect=0;
+var rights = [];
+var wrongs = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8"];
+var answers = ["","Finland","500","Honduras","Starbucks","Tim Horton","Black Coffee","2","Milk"];
+var x, seconds;
 
-$("#q1").on("submit", function(e){
-    e.preventDefault();
-   var value = $('input[name="q1"]:checked').val();
-   console.log(value);
-   if (value === "Finland"){
-       rigths.push(value);
-       correct++;
 
-   }else{
-        incorrect++
-        wrongs.push(value);
-   };
-   console.log(incorrect);
-   console.log(correct);
-  });
+$(document).ready(function() {
+    timer();
+       
+    function review (q){
+        
+        var value = $('input[name="' + q + '"]:checked').val();
+        console.log(value);
 
-$("#q2").on("submit", function(e){
-  e.preventDefault();
- var value = $('input[name="q2"]:checked').val();
- console.log(value);
- if (value === "500"){
-    rigths.push(value);
-    correct++;
+        var str = q;
+        var a = str.slice(1, 2);
+        a = parseInt(a);
+        console.log(a);
 
+        var indexRights = rights.indexOf(q);
+        if (indexRights > -1) {
+          rights.splice(indexRights, 1);
+        }
+
+        var indexWrongs = wrongs.indexOf(q);
+        if (indexWrongs > -1) {
+          wrongs.splice(indexWrongs, 1);
+        }
+     
+        if (value === answers[a]){
+            rights.push(q);
+ 
+        }else{
+             wrongs.push(q);
+   
+        };
+
+        console.log(rights);
+        console.log(wrongs);        
     }
-    else{
-    incorrect++
-    wrongs.push(value);
-};
-console.log(incorrect);
-console.log(correct);
+
+    $("#q1").on("click", function(e){
+        review("q1");
+    });
+
+    $("#q2").on("click", function(e){
+        review("q2");
+ 
+    });
+
+    $("#q3").on("click", function(e){
+        review("q3");
+     });
+
+    $("#q4").on("click", function(e){
+        review("q4");   
+    });
+
+    $("#q5").on("click", function(e){
+        review("q5");
+    });
+
+    $("#q6").on("click", function(e){
+        review("q6");
+    });
+
+    $("#q7").on("click", function(e){
+        review("q7");   
+    });
+
+    $("#q8").on("click", function(e){
+        review("q8");
+    });
+
+    $("#submit").on("click", function(e){
+
+        e.preventDefault();
+        document.getElementById("time").innerHTML = "You submitted!";
+        clearInterval(x);
+
+        if (rights.length > wrongs.length) {
+
+                $("#questions").html('<h1 style="color: orange;"> You are a WINNER <h1>');
+
+
+        } else {
+                $("#questions").html('<h1 style="color:red;"> You are a LOSER <h1>');
+
+        }
+    });  
 });
 
-$("#q3").on("submit", function(e){
-    e.preventDefault();
-   var value = $('input[name="q3"]:checked').val();
-   console.log(value);
-   console.log(value);
- if (value === "Honduras"){
-    rigths.push(value);
-    correct++;
 
-    }
-else{
-    incorrect++
-    wrongs.push(value);
-};
-console.log(incorrect);
-console.log(correct);
-  });
+function timer() {
 
-  $("#q4").on("submit", function(e){
-    e.preventDefault();
-   var value = $('input[name="q4"]:checked').val();
-   console.log(value);
-   if (value === "Starbucks"){
-    rigths.push(value);
-    correct++;
+        seconds = 3*60;    
+        x = setInterval(function()
+        {
+            document.getElementById("time").innerHTML = Math.floor(seconds/60) + " m " + seconds%60 + " s";
+           
+            seconds--;
+                       
+            if (seconds === 0) {
+                document.getElementById("time").innerHTML = "Time's Up!";
+                $("#submit").hide();
+                $("input[type=radio]").prop('disabled', true);               
+                clearInterval(x);
+            }
 
-    }else{
-     incorrect++
-     wrongs.push(value);
-    };
-    console.log(incorrect);
-    console.log(correct);
-  });
+        }, 1000);    
+}
 
 
-  $("#q5").on("submit", function(e){
-    e.preventDefault();
-   var value = $('input[name="q5"]:checked').val();
-   console.log(value);
-   console.log(value);
- if (value === "Tim Horton"){
-    rigths.push(value);
-    correct++;
 
-    }
-else{
-    incorrect++
-    wrongs.push(value);
-};
-console.log(incorrect);
-console.log(correct);
-  });
-
-  $("#q6").on("submit", function(e){
-    e.preventDefault();
-   var value = $('input[name="q6"]:checked').val();
-   console.log(value);
-   console.log(value);
- if (value === "Black Coffee"){
-    rigths.push(value);
-    correct++;
-
-    }
-else{
-    incorrect++
-    wrongs.push(value);
-};
-console.log(incorrect);
-console.log(correct);
-  });
-
-  $("#q7").on("submit", function(e){
-    e.preventDefault();
-   var value = $('input[name="q7"]:checked').val();
-   console.log(value);
-   console.log(value);
- if (value === "2"){
-    rigths.push(value);
-    correct++;
-
-    }
-else{
-    incorrect++
-    wrongs.push(value);
-};
-console.log(incorrect);
-console.log(correct);
-  });
-
-  $("#q8").on("submit", function(e){
-    e.preventDefault();
-   var value = $('input[name="q8"]:checked').val();
-   console.log(value);
-   console.log(value);
- if (value === "Milk"){
-    rigths.push(value);
-    correct++;
-
-    }
-else{
-    incorrect++
-    wrongs.push(value);
-};
-console.log(incorrect);
-console.log(correct);
-  });
-
-  console.log(rigths);
-  console.log(wrongs);
-
+$("#restart").on("click", function restart (event)
+{
+    location.reload();
+});
